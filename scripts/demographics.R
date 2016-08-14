3bbb917af60486ee0c5522ad9e2c679b367e6afa
Thomas Guignard <tom@timtom.ch>
2016-08-14 18:29:18 -0400

    Added base competencies analysis

--- scripts/demographics.R
+++ scripts/demographics.R
@@ -14,6 +14,8 @@ survey <- read.csv(infile)
 # Replace the long names of the columns by just the question codes
 # E.g. "X1..My.question" is replaced by "X1" in the column names.
 names(survey) <- gsub("^(.*?)\\.\\.(.*)", "\\1", colnames(survey))
+# TO DO: salvage the full question names somewhat
+# so as not to depend on survey keys for question names.
 
 # Create a Word document for analyzed data (to copy-paste into thesis)
 library(rtf)
@@ -31,6 +33,7 @@ library("ggplot2")
 # Not very useful, replace it by something better.
 #ggplot(data=survey, aes(x = X1)) + geom_bar(stat="count") + coord_flip() + ylab("Number of responses") + ggtitle("Figure 1: Responses by type of library")
 
+survey_demographics <- survey[c(1,2,4)]
 # This is better, a simple contingency table across library type and size:
 demog_table <- with(survey_demographics,table(X1,X2, dnn=c("Type of institution", "Total staff (FTE)")))
 
@@ -51,6 +54,15 @@ addTable(rtffile, cbind(rownames(demog_results), demog_results))
 
 print("Response demographics:")
 print(demog_results)
+
+# Extract base competencies data
+survey_basecomp = survey[6:15]
+basecomp_results <- t(sapply(survey_basecomp, function(x) table(factor(x, levels=levels(unlist(survey_basecomp))))))
+rownames(basecomp_results) <- c("Project Management", "Accounting/costing/budgeting", "Communication/outreach", "Accessibility assessment", "Negotiation", "User Experience design", "Graphic design", "Web design", "Computer programming", "System admin.")
+print(basecomp_results)
+
+addParagraph(rtffile, "Base competencies:")
+addTable(rtffile, cbind(rownames(basecomp_results), basecomp_results))
 
 # Close and write out the RTF file
 done(rtffile)
