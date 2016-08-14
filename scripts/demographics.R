1eededb561fdfb6dfb28d1fc0d28449fca4982b8
Thomas Guignard <tom@timtom.ch>
2016-08-14 10:49:11 -0400

    Finished demographics output

--- scripts/demographics.R
+++ scripts/demographics.R
@@ -1,5 +1,15 @@
+# This script takes the CSV file exported from BOS with the survey data
+
+# Specify which file to open
+infile <- "data/results-for-investigating-2016-08-13-2209.csv"
+
+# And where to store results
+plotspath <- "plots"
+tablespath <- "tables"
+
+
 # Read survey data
-survey <- read.csv("data/results-for-investigating-2016-08-13-2209.csv")
+survey <- read.csv(infile)
 
 # Replace the long names of the columns by just the question codes
 # E.g. "X1..My.question" is replaced by "X1" in the column names.
@@ -7,11 +17,12 @@ names(survey) <- gsub("^(.*?)\\.\\.(.*)", "\\1", colnames(survey))
 
 # Create a Word document for analyzed data (to copy-paste into thesis)
 library(rtf)
-rtffile <- RTF("tables/demographics.doc")
+rtffile <- RTF(paste(tablespath,"demographics.doc", sep = "/"))
+addHeader(rtffile, "Survey demographics", subtitle = paste("Created:", Sys.time(), "from file:", infile))
 
 # Output total number of responses
-print(paste('Total number of responses: ', nrow(survey)))
-addParagraph(rtffile, paste('Total number of responses: ', nrow(survey)))
+print(paste('Total number of responses:', nrow(survey)))
+addParagraph(rtffile, paste('Total number of responses:', nrow(survey)))
 
 # Load the ggplot2 library, for plotting results.
 library("ggplot2")
@@ -22,11 +33,24 @@ library("ggplot2")
 
 # This is better, a simple contingency table across library type and size:
 demog_table <- with(survey_demographics,table(X1,X2, dnn=c("Type of institution", "Total staff (FTE)")))
+
+# Add totals and percentage
+demog_results <- data.frame(matrix(demog_table, nrow=nrow(demog_table)))
+colnames(demog_results) = dimnames(demog_table)[[2]]
+rownames(demog_results) = dimnames(demog_table)[[1]]
+demog_results$Total = rowSums(demog_results)
+demog_results$Percentage = round(demog_results$Total/sum(demog_results$Total)*100)
+results_row <- nrow(demog_results)+1
+demog_results[results_row,] <- colSums(demog_results)
+rownames(demog_results)[results_row] <- "Total"
+
+# TO FIX: Add proper labels for type and size of institution
+# Also sort by percentage
 addParagraph(rtffile, "Survey demographics:")
-addTable(rtffile, demog_table)
+addTable(rtffile, cbind(rownames(demog_results), demog_results))
 
 print("Response demographics:")
-print(demog_table)
+print(demog_results)
 
 # Close and write out the RTF file
 done(rtffile)
